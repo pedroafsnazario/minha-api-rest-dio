a6de53920460d61d7f5a2bbadda16e0c83f7587a
pedroafsnazario <pedro905fernades@gmail.com>
2023-10-17 15:27:12 -0300

    Inicialização do projeto API

---
+++ src/main/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplication.java
@@ -0,0 +1,13 @@
+package eu.dio.minhaapirestdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApiRestDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaApiRestDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dio.minhaapirestdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
