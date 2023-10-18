dfca1775376502840231dca74b22e7feb6c02c0d
pedroafsnazario <pedro905fernades@gmail.com>
2023-10-18 15:59:12 -0300

    Correção de CORS

--- src/main/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplication.java
+++ src/main/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplication.java
@@ -2,7 +2,7 @@ package eu.dio.minhaapirestdio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class MinhaApiRestDioApplication {
 
