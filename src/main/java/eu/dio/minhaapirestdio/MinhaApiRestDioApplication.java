f054f955d34c817a868dc075b1dc2f42905ed44f
pedroafsnazario <pedro905fernades@gmail.com>
2023-10-18 16:11:10 -0300

    Correção de CORS.2 (annotations fornecidas em aula não funcionam/não existem.)

--- src/main/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplication.java
+++ src/main/java/eu/dio/minhaapirestdio/MinhaApiRestDioApplication.java
@@ -2,6 +2,9 @@ package eu.dio.minhaapirestdio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class MinhaApiRestDioApplication {
